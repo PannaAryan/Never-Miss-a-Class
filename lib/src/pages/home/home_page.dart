cd38fedeed54d14b9d14d6a5f7a6f6fda6177a44
PannaAryan <panna57@student.sust.edu>
2023-02-08 03:08:18 +0600

    linking button

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../groups/group_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -16,9 +18,43 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Center(
           child: TextButton(
-            onPressed: () {
-              print("Pressed? You sure?");
-            },
+            onPressed: () => showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                      title: const Text("Please enter your name"),
+                      content: TextFormField(),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            "Cancel",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => GroupPage(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Enter",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
             child: const Text(
               "initiate group chat",
               style: TextStyle(
