ffe99eefe94f44c54db95497019175cfa8042828
PannaAryan <panna57@student.sust.edu>
2023-03-12 19:10:29 +0600

    backend connect

--- lib/src/pages/groups/group_page.dart
+++ lib/src/pages/groups/group_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class GroupPage extends StatefulWidget {
   final String name;
@@ -9,45 +10,76 @@ class GroupPage extends StatefulWidget {
 }
 
 class _GroupPageState extends State<GroupPage> {
+  IO.Socket? socket;
+  // ignore: non_constant_identifier_names
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Anonymous Group"),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Type here....",
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 2,
+  void initState() {
+    super.initState();
+    connect();
+  }
+
+  void connect() {
+    socket = IO.io("http://localhost:3000", <String, dynamic>{
+      "transports": ["websocket"],
+      "autoConnect": false,
+    });
+
+    socket!.connect();
+    print("We are Here");
+    socket!.onConnect((_) {
+      print('connect');
+      socket!.emit('msg', 'test');
+    });
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Anonymous Group"),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Type here....",
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.send,
-                    color: Colors.teal,
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.send,
+                      color: Colors.teal,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
   }
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    newMethod();
+  }
+
+  Never newMethod() => throw UnimplementedError();
 }
